65a4aa334ac3debb6b37ab6955903c9919235303
tienmanh240503-cloud <tienmanh240503@gmail.com>
2025-11-24 20:32:40 +0700

    fix Layout

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "../components/Header/Header";
-import { Outlet } from "react-router";
+import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer/Footer";
 import MedicalChatBox from "../components/MedicalChatBox/MedicalChatBox";
 import ScrollToTop from "../components/ScrollToTop/ScrollToTop";
